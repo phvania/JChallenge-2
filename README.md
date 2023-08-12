66bf8d137e3cc37938fc14ed8fc79d02052b2caf
phvania <129224910+phvania@users.noreply.github.com>
2023-08-12 17:43:09 -0400

    push

--- README.md
+++
@@ -1,1 +0,0 @@
-# JChallenge-2
--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  console.log("Hey! You clicked the button!");
+  console.log("");
+ // return 'generated password will go Here;'
 }
 //prompt the user for the password criteria
 //password leanth 8 to 128
@@ -10,9 +11,8 @@ function generatePassword() {
 //validate the input 
 //display the generated  password on the  page
 
-return ' generated password will go here';
+ 
   
-
 
 
 
@@ -25,33 +25,67 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+ passwordText.value = password;
 
 }
  
-
-  var password = prompt('what is your password?', '') ;
-  var length = 8;
+//declered variable//prompt//confirm
+ 
+  var  passwordlength = prompt("Your password length will be 8 to 128 character");
+  alert ( "your password should contain uppercase, lowercase, number, and symbol");
+  var confirm = confirm("My password length will be 8.");
   var  includeUppercase =  "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var  includeLowercase = "abcdefghijklmnopqrstuvwxyz";
   var  includeNumber = "1234567890";
   var includeSymbol = "! $ %  + # _~ /" ;
+  var allchars =  "upperCase + lowerCase +number + symbol";
+  
+  //ask for password criteria
+  //var carType = prompt("what type of password criteria would you like to use?");
+  //var carType = prompt("Enter a character type: uppercase, lowercase, number,symbol");
 
-  var  allchars = upperCase + lowerCase +number + symbol;
+  
+  //generate password
 
    function generatePassword() {
+    //evaluate character type
     var password = "";
-    password += includeUppercase[ Math.floor(Math.random() * includeUppercase .length)];
-    password += includeLowercase[ Math.floor( Math.random() * includeLowercase.length)];
-    password += includeNumber[ Math.floor( Math.random() * includeNumber.length)];
-    password += includeSymbol[ Math.floor( Math.random() * includeSymbol.length)];
+   console.log( password += includeUppercase [Math.floor(Math.random() * includeUppercase .length)]);
 
+   console.log(password += includeLowercase [ Math.floor( Math.random() * includeLowercase.length)]);
+    console.log(password += includeNumber [ Math.floor( Math.random() * includeNumber.length)]);
+    console.log(password += includeSymbol [ Math.floor( Math.random() * includeSymbol.length)]);
+    console.log(password += includeLowercase[Math.floor(Math.random() * includeLowercase.length)]);
+    console.log(password += includeLowercase[Math.floor(Math.random() * includeLowercase.length)]);
+   console.log(password += includeLowercase[Math.floor(Math.random() * includeLowercase.length)]);
+   console.log(password += includeNumber[Math.floor(Math.random() * includeNumber.length)]);
+   return password;
+  
    }
-while( length> password.length){
-  password += allchars[ Math.floor( Math.random() * allchars.length)];
-}
 
-passwordText.value = password;
+  
+   var password = "";
+   for ( var i = 0; i < passwordlength; i++) {
+    password += allchars.charAt( Math.floor(Math.random() * allchars.length));
+   }
+   generateBtn.value = password;
+     password;
+
+   alert( generatePassword());
+
+  
+   
+   
+   
+   
+   
+
+
+
+
+
+
+
 
 
 
@@ -59,5 +93,18 @@ passwordText.value = password;
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword) password += includeUppercase[ Math.floor(Math.random() * includeUppercase .length)];
+generateBtn.addEventListener("click", writePassword)
+let button = document.querySelector("#generate");
+
+    //function generate(){
+     // let password = "";
+      //password += UpperCase [Math.floor(Math.random() * UpperCase.length)];    
+        //password += lowerCaseCase [Math.floor(Math.random() * lowerCase.length)];     
+        // password += number [Math.floor(Math.random() * number.length)];     
+          //password += symbol [Math.floor(Math.random() * symbol.length)];
+
+
+
+
+
     
