33dc24f8e8adbaabac270305b8a760e6827ded55
phvania <129224910+phvania@users.noreply.github.com>
2023-10-19 23:02:04 -0400

    updated script file and readme

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# JChallenge-2
+## TITLE---- Password Generator
+
+## Description:
+An  application that enables employees to generate random passwords based on criteria that they’ve selected. This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean and polished, responsive user interface that adapts to multiple screen sizes.The password can include special characters.
+
+## This challenge involved generating a random password based on a set of user selections using the instructions below
+
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+
+## Screenshort:
+
+![Alt text](image-1.png)
+
+## Link to deployed:
+
+ https://phvania.github.io/JChallenge-2
+
+https://github.com/phvania/JChallenge-2
+
+https://github.com/phvania/JChallenge-2.git
+
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,110 +1,93 @@
-
-var generateBtn = document.querySelector("#generate");
-
+// Assignment code here
 function generatePassword() {
-  console.log("");
- // return 'generated password will go Here;'
-}
-//prompt the user for the password criteria
-//password leanth 8 to 128
-//uppercase, lowercase, number, special character
-//validate the input 
-//display the generated  password on the  page
+  var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  var uppercaseCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  var lowercaseCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  var specialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
+  var possibleCharacters = [];
 
- 
-  
+  // get input and validate
+  numberOfCharacters = prompt("How many characters do you want in your password? Choose between 8-128 characters.");
+  if (numberOfCharacters < 8 || numberOfCharacters > 128) {
+    return "Please choose a valid number of characters.";
+  } else if (isNaN(numberOfCharacters)) {
+    numberOfCharacters = prompt("Please enter a valid number.");
+  }
+  else {
+    alert("Your password will be " + numberOfCharacters + " characters long.");
+    
+  }
 
+  hasLowercase = confirm("Do you want lowercase characters?");
+  if (hasLowercase) {
+    var turnToLowercase = alert("Your password will have lowercase characters.");
+  }
+  else {
+    alert("Your password will NOT have lowercase characters.");
+  }
 
+  hasUppercase = confirm("Do you want uppercase characters?");
+  if (hasUppercase) {
+    alert("Your password will have uppercase characters.");
+  }
+  else {
+    alert("Your password will NOT have uppercase characters.");
+  }
 
+  hasNumbers = confirm("Do you want to use numbers?");
+  if (hasNumbers) {
+    alert("Your password will have numbers.");
+  }
+  else {
+    alert("Your password will NOT have numbers.");
+  }
 
-//Assignment Code
+  hasSpecial = confirm("Do you want special characters?");
+  if (hasSpecial) {
+    alert("Your password will have special characters.");
+  }
+  else {
+    alert("Your password will NOT have special characters.");
+  }
+
+  if (hasLowercase === false && hasUppercase === false && hasNumbers === false && hasSpecial === false) {
+    return "Please select at least one character type.";
+  };
+
+  // group selected characters
+  if (hasLowercase) {
+    possibleCharacters = possibleCharacters.concat(lowercaseCharacters);
+  }
+  if (hasUppercase) {
+    possibleCharacters = possibleCharacters.concat(uppercaseCharacters);
+  }
+  if (hasNumbers) {
+    possibleCharacters = possibleCharacters.concat(numericCharacters);
+  }
+  if (hasSpecial) {
+    possibleCharacters = possibleCharacters.concat(specialCharacters);
+  }
+
+  // pick random cards out of new pool for length of password
+  let finalPassword = ""
+  for (let i = 0; i < numberOfCharacters; i++) {
+    let rng =[Math.floor(Math.random() * possibleCharacters.length)];
+    // or finalPassword += possibleCharacters[rng];
+    finalPassword = finalPassword + possibleCharacters[rng];
+  }
+  return finalPassword;
+};
+
+// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
- passwordText.value = password;
+  passwordText.value = password;
 
 }
- 
-//declered variable//prompt//confirm
- 
-  var  passwordlength = prompt("Your password length will be 8 to 128 character");
-  alert ( "your password should contain uppercase, lowercase, number, and symbol");
-  var confirm = confirm("My password length will be 8.");
-  var  includeUppercase =  "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var  includeLowercase = "abcdefghijklmnopqrstuvwxyz";
-  var  includeNumber = "1234567890";
-  var includeSymbol = "! $ %  + # _~ /" ;
-  var allchars =  "upperCase + lowerCase +number + symbol";
-  
-  //ask for password criteria
-  //var carType = prompt("what type of password criteria would you like to use?");
-  //var carType = prompt("Enter a character type: uppercase, lowercase, number,symbol");
-
-  
-  //generate password
-
-   function generatePassword() {
-    //evaluate character type
-    var password = "";
-   console.log( password += includeUppercase [Math.floor(Math.random() * includeUppercase .length)]);
-
-   console.log(password += includeLowercase [ Math.floor( Math.random() * includeLowercase.length)]);
-    console.log(password += includeNumber [ Math.floor( Math.random() * includeNumber.length)]);
-    console.log(password += includeSymbol [ Math.floor( Math.random() * includeSymbol.length)]);
-    console.log(password += includeLowercase[Math.floor(Math.random() * includeLowercase.length)]);
-    console.log(password += includeLowercase[Math.floor(Math.random() * includeLowercase.length)]);
-   console.log(password += includeLowercase[Math.floor(Math.random() * includeLowercase.length)]);
-   console.log(password += includeNumber[Math.floor(Math.random() * includeNumber.length)]);
-   return password;
-  
-   }
-
-  
-   var password = "";
-   for ( var i = 0; i < passwordlength; i++) {
-    password += allchars.charAt( Math.floor(Math.random() * allchars.length));
-   }
-   generateBtn.value = password;
-     password;
-
-   alert( generatePassword());
-
-  
-   
-   
-   
-   
-   
-
-
-
-
-
-
-
-
-
-
-
-
-
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword)
-let button = document.querySelector("#generate");
+generateBtn.addEventListener("click", writePassword);
 
-    //function generate(){
-     // let password = "";
-      //password += UpperCase [Math.floor(Math.random() * UpperCase.length)];    
-        //password += lowerCaseCase [Math.floor(Math.random() * lowerCase.length)];     
-        // password += number [Math.floor(Math.random() * number.length)];     
-          //password += symbol [Math.floor(Math.random() * symbol.length)];
-
-
-
-
-
-    
